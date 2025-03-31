a63df0e9eebbe54607c965816fe47ea2d6d039e9
Cameron Evans <camerondaneevans@gmail.com>
2025-03-30 22:52:09 -0600

    done but buggy

--- image_to_stitches/src/main.rs
+++ image_to_stitches/src/main.rs
@@ -3,22 +3,66 @@ mod ssim;
 use image::ImageReader as ImageReader;
 use std::path::Path;
 use fixedbitset::FixedBitSet;
+use rand::Rng;
 
 
 const RESOLUTION: usize = 512;
 const NAIL_COUNT: usize = 200;
-// const CANVAS_DIAMETER: u32 = 1000000; // micrometers
-// const THREAD_DIAMETER: u32 = 500; // micrometers
+const CANVAS_DIAMETER: u32 = 1000000; // micrometers
+const THREAD_DIAMETER: u32 = 500; // micrometers
 
-fn genetic_simulation(canvas:&string_canvas::StringCanvas, target_image:&Vec<u8>, parent_count: usize) {
-    // Initialize population
+fn genetic_simulation(canvas:&string_canvas::StringCanvas, target_image:&Vec<u8>, parent_count: usize, generation_count: usize, mutation_chance: f64) {
+    let mut rng = rand::rng();
+    let ink_scalar: f64 = (THREAD_DIAMETER as f64 * RESOLUTION as f64) / CANVAS_DIAMETER as f64;
     let pop_size = parent_count + ((parent_count * (parent_count - 1)) / 2);
-    let mut population = vec![FixedBitSet::with_capacity(canvas.count); pop_size];
-
-    loop {
-        //
+    let mut population = vec![(-1.0 as f64, FixedBitSet::with_capacity(canvas.count)); pop_size]; //fitness, stitch_key
+    for i in 0..pop_size {
+        population[i].1 = FixedBitSet::with_capacity(canvas.count);
+        for j in 0..canvas.count {
+            population[i].1.set(j, rand::random());
+        }
     }
 
+    for _gen in 0..generation_count {
+        for individual in &mut population {
+            let mut drawing = vec![0 as u8; target_image.len()];
+            let mut source: usize = 0;
+            let mut target: usize = 1;
+            let mut step: usize = 0;
+            for index in individual.1.ones() {
+                for _ in 0..index-step {
+                    target += 1;
+                    if target == NAIL_COUNT {
+                        source += 1;
+                        target = source + 2;
+                    }
+                }
+                for (flat_index, ink) in canvas.get_stitch(source, target).into_iter() {
+                    drawing[*flat_index] += (ink * ink_scalar) as u8;
+                }
+                step = index;
+            }
+            individual.0 = ssim::ssim(&drawing, target_image, RESOLUTION, 11, 0.01, 0.03);
+        }
+
+        population.sort_by(|a, b| a.0.partial_cmp(&b.0).unwrap());
+        println!("Generation {}: Best fitness: {}", _gen, population[0].0);
+        //keep parent_count best parents for next generation and cross-breed the rest
+        for i in 0..parent_count {
+            for j in i+1..parent_count {
+                let mut child = (-1.0 as f64, population[i].1.clone());
+                for k in 0..canvas.count {
+                    if rng.random_bool(0.5) {
+                        child.1.set(k, population[j].1[k]);
+                    }
+                    if rng.random_bool(mutation_chance) {
+                        child.1.set(k, !child.1[k]);
+                    }
+                }
+                population[(i*parent_count)+j+parent_count] = child;
+            }
+        }
+    }
 }
 
 fn load_image_as_bytes(path: &Path) -> Result<Vec<u8>, image::ImageError> {
@@ -42,19 +86,8 @@ fn main() {
     match load_image_as_bytes(path) {
         Ok(bytes) => {
             println!("Image converted to {} bytes", bytes.len());
-            genetic_simulation(&_canvas, &bytes, 10);
+            genetic_simulation(&_canvas, &bytes, 10, 10, 0.0001);
         }
         Err(e) => eprintln!("Error: {}", e),
     }
 }
-
-// fitness DONE
-// precompute DONE
-
-//loop
-    //make generation
-    //evaluate fitness
-    //select parents
-    //crossover
-    //mutate
-    //replace population
--- image_to_stitches/src/string_canvas.rs
+++ image_to_stitches/src/string_canvas.rs
@@ -3,35 +3,36 @@ use bresenham::Bresenham;
 
 
 pub struct StringCanvas {
-    resolution: usize,
-    nail_count: usize,
-    stitch_cache: HashMap<usize, HashMap<usize, HashMap<usize, u8>>>, // index1, index2, (flat_index, ink)
+    _resolution: usize,
+    _nail_count: usize,
+    stitch_cache: HashMap<usize, HashMap<usize, HashMap<usize, f64>>>, // index1, index2, (flat_index, ink)
     pub count: usize
 }
 
 impl StringCanvas {
-    pub fn new(resolution: usize, nail_count: usize) -> Self {
+    pub fn new(_resolution: usize, _nail_count: usize) -> Self {
         let mut stitch_cache = HashMap::new();
-        let radius = resolution as f64 / 2.0;
+        let radius = _resolution as f64 / 2.0;
         let pi = std::f64::consts::PI;
         let mut count = 0;
-        for i1 in 0..nail_count {
+        for i1 in 0.._nail_count {
             let mut middle_map = HashMap::new();
-            let radians1 = 2.0 * pi * i1 as f64 / nail_count as f64;
+            let radians1 = 2.0 * pi * i1 as f64 / _nail_count as f64;
             let xy1 = (radius * radians1.cos(), radius * radians1.sin());
-            for j in 2..(nail_count+1)/2 {
+            for j in 2..(_nail_count+1)/2 {
                 let mut inner_map = HashMap::new();
-                let i2 = (i1 + j) % nail_count;
-                let radians2 = 2.0 * pi * i2 as f64 / nail_count as f64;
+                let i2 = (i1 + j) % _nail_count;
+                let radians2 = 2.0 * pi * i2 as f64 / _nail_count as f64;
                 let xy2 = (radius * radians2.cos(), radius * radians2.sin());
                 let true_distance = ((xy1.0 - xy2.0).powi(2) + (xy1.1 - xy2.1).powi(2)).sqrt();
                 let bresenham: Vec<_> = Bresenham::new(
                     (xy1.0 as isize, xy1.1 as isize),
                     (xy2.0 as isize, xy2.1 as isize)
                 ).collect();
-                let ink = (255.0 * bresenham.len() as f64 / true_distance) as u8; // POTENTIAL FOR IMPROVEMENT, BASE INK ON DISTANCE TO TRUE LINE
+                let ink = 255.0 * bresenham.len() as f64 / true_distance; // POTENTIAL FOR IMPROVEMENT, BASE INK ON DISTANCE TO TRUE LINE
                 for (x, y) in bresenham {
-                    let flat_index = ((x + radius as isize) + ((y - radius as isize) * -1)) as usize * resolution; // Wrong??
+                    let flat_index = ((x + radius as isize) + ((y - radius as isize) * -1)) as usize * _resolution;
+                    assert!(flat_index < _resolution * _resolution);
                     inner_map.insert(flat_index, ink);
                 }
                 middle_map.insert(i2, inner_map);
@@ -40,14 +41,14 @@ impl StringCanvas {
             stitch_cache.insert(i1, middle_map);
         }
         StringCanvas {
-            resolution,
-            nail_count,
+            _resolution,
+            _nail_count,
             stitch_cache,
             count
         }
     }
 
-    pub fn print_stitch_cache(&self) {
+    pub fn _print_stitch_cache(&self) {
         for (i1, middle_map) in &self.stitch_cache {
             println!("Nail {}: ", i1);
             for (i2, inner_map) in middle_map {
@@ -59,7 +60,7 @@ impl StringCanvas {
         }
     }
 
-    pub fn get_stitch(&self, source: usize, target: usize) -> &HashMap<usize, u8> {
+    pub fn get_stitch(&self, source: usize, target: usize) -> &HashMap<usize, f64> {
         if let Some(middle_map) = self.stitch_cache.get(&source) {
             if let Some(inner_map) = middle_map.get(&target) {
                 return inner_map;
